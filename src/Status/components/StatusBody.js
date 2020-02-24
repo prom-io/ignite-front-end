21272fd2761c2e836c648c7586e2c505527c41e6
PaperPlane01 <dagfortnewage@gmail.com>
2020-02-24 13:37:53 +0600

    fix: (24.02.20) Status text was being truncated on overflow

--- src/Status/components/StatusBody.js
+++ src/Status/components/StatusBody.js
@@ -1,10 +1,22 @@
 import React from "react";
-import {CardContent, Typography} from "@material-ui/core";
+import {CardContent, Typography, makeStyles} from "@material-ui/core";
 
-export const StatusBody = ({text}) => (
-    <CardContent className="status-list-body-container">
-        <Typography variant="body1">
-            {text}
-        </Typography>
-    </CardContent>
-);
+const useStyles = makeStyles(() => ({
+    statusText: {
+        overflowWrap: "break-word"
+    }
+}));
+
+export const StatusBody = ({text}) => {
+    const classes = useStyles();
+
+    return (
+        <CardContent className="status-list-body-container">
+            <Typography variant="body1"
+                        className={classes.statusText}
+            >
+                {text}
+            </Typography>
+        </CardContent>
+    );
+};

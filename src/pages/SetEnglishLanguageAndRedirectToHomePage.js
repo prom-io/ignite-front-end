57732350f2488ba44575b33ce666988fe122b749
PaperPlane01 <dagfortnewage@gmail.com>
2020-05-11 18:39:14 +0600

    feat: (11.05.20) Added pages which set language and redirect to main page

---
+++ src/pages/SetEnglishLanguageAndRedirectToHomePage.js
@@ -0,0 +1,17 @@
+import React from "react";
+import {inject, observer} from "mobx-react";
+import {Routes} from "../routes";
+
+const _SetEnglishLanguageAndRedirectToHomePage = ({localization, routerStore}) => {
+    localization.setSelectedLanguage("en");
+    routerStore.router.goTo(Routes.home);
+
+    return null;
+};
+
+const mapMobxToProps = ({localization, store}) => ({
+    localization,
+    routerStore: store
+});
+
+export const SetEnglishLanguageAndRedirectToHomePage = inject(mapMobxToProps)(observer(_SetEnglishLanguageAndRedirectToHomePage));

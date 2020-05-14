1f889a9c664f972ea3f62b94db8e9ef51d2fa632
PaperPlane01 <dagfortnewage@gmail.com>
2020-05-14 16:15:16 +0600

    fix: (14.05.20) Fixed username regexp to accept Chinese characters

--- src/User/validation/index.js
+++ src/User/validation/index.js
@@ -1,6 +1,6 @@
 import {isStringEmpty} from "../../utils/string-utils";
 
-const USERNAME_REGEXP = /^[a-zA-Z0-9\u3130-\u318F\uAC00-\uD7AF_]+$/;
+const USERNAME_REGEXP = /^[a-zA-Z0-9\u3130-\u318F\uAC00-\uD7AF\u4E00-\u9FFF\u3400-\u4DBF\u20000-\u2A6DF\u2A700-\u2B73F\u2B740-\u2B81F\u2B820-\u2CEAF\uF900-\uFAFF\u2F800-\u2FA1F_]+$/;
 
 export const validateUsername = username => {
     if (isStringEmpty(username)) {

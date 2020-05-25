895cb8ba758670a0d76cc6bd9676c224e7d7e280
PaperPlane01 <dagfortnewage@gmail.com>
2020-05-25 17:54:26 +0600

    fix: (25.05.20) Fixed infinite scrolling of statuses list

--- src/Status/components/StatusList.js
+++ src/Status/components/StatusList.js
@@ -1,6 +1,15 @@
-import React, { Fragment, useEffect } from 'react';
-import { Typography } from '@material-ui/core';
+import React, { Fragment } from 'react';
+import { CircularProgress, Typography, makeStyles } from '@material-ui/core';
+import InfiniteScroll from 'react-infinite-scroll-component';
 import { StatusListItem } from './StatusListItem';
+
+const useStyles = makeStyles(() => ({
+    centered: {
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        display: 'table',
+    },
+}));
 
 export const StatusList = ({
     statuses,
@@ -15,40 +24,35 @@ export const StatusList = ({
     header,
     hideThreadLinks,
 }) => {
-    const trackScrolling = () => {
-        const element = document.getElementById('statusList');
-
-        if (element.getBoundingClientRect().bottom <= window.innerHeight) {
-            onNextPageRequest();
-        }
-    };
-
-
-    useEffect(() => {
-        document.addEventListener('scroll', trackScrolling);
-
-        return () => document.removeEventListener('scroll', trackScrolling);
-    });
+    const classes = useStyles({radius: 55});
 
     return (
         <div id="statusList" className="status-list-card paddingBottomRoot">
             {header && statuses.length !== 0 && <Typography variant="h6">{header}</Typography>}
-            {statuses.map(status => (
-                <Fragment key={status.id}>
-                    <StatusListItem
-                        status={status}
-                        onFavouriteStatusChange={onFavouriteClick}
-                        onFollowRequest={onFollowRequest}
-                        onUnfollowRequest={onUnfollowRequest}
-                        displayMenu={displayMenu}
-                        currentUserIsAuthor={currentUser && currentUser.id === status.account.id}
-                        statusLikePending={statusLikePendingMap[status.id]}
-                        repostPending={repostsPendingMap[status.id]}
-                        link
-                        hideThreadLink={hideThreadLinks}
-                    />
-                </Fragment>
-            ))}
+            <InfiniteScroll
+                next={onNextPageRequest}
+                hasMore
+                loader={<CircularProgress size={15} color="primary" className={classes.centered} />}
+                dataLength={statuses.length}
+                style={{ overflowY: 'hidden' }}
+            >
+                {statuses.map(status => (
+                    <Fragment key={status.id}>
+                        <StatusListItem
+                            status={status}
+                            onFavouriteStatusChange={onFavouriteClick}
+                            onFollowRequest={onFollowRequest}
+                            onUnfollowRequest={onUnfollowRequest}
+                            displayMenu={displayMenu}
+                            currentUserIsAuthor={currentUser && currentUser.id === status.account.id}
+                            statusLikePending={statusLikePendingMap[status.id]}
+                            repostPending={repostsPendingMap[status.id]}
+                            link
+                            hideThreadLink={hideThreadLinks}
+                        />
+                    </Fragment>
+                ))}
+            </InfiniteScroll>
         </div>
     );
 };

82a75f69088e7f0f47b11920c408f13f6569eaaf
Илья Долженко <ilay00@mail.ru>
2020-06-02 13:10:30 +0300

    some fixes

--- src/Follow/components/FollowPeopleItem.js
+++ src/Follow/components/FollowPeopleItem.js
@@ -1,103 +1,102 @@
-import React from 'react';
-import { inject } from 'mobx-react';
-import { Avatar, Typography, makeStyles } from '@material-ui/core';
-import { Link } from 'mobx-router';
+import React from "react";
+import { inject } from "mobx-react";
+import { Avatar, Typography, makeStyles } from "@material-ui/core";
+import { Link } from "mobx-router";
 
-import { trimString } from '../../utils/string-utils';
-import { Routes } from '../../routes';
-import { FollowButton } from '.';
+import { trimString } from "../../utils/string-utils";
+import { Routes } from "../../routes";
+import { FollowButton } from "./";
+import { ClickEventPropagationStopper } from "../../ClickEventProgatationStopper";
 
 const useStyles = makeStyles(theme => ({
     followPeopleItem: {
-        display: 'flex',
+        display: "flex",
         padding: 16,
-        border: '1px solid #F1EBE8',
-        borderTop: 'none',
+        border: "1px solid #F1EBE8",
+        borderTop: "none",
+        textDecoration: "none",
+
+        "&:hover": {
+            background: "#FFFBF8"
+        }
     },
     followPeopleItemAvatar: {
         marginRight: 12,
         height: 35,
-        width: 35,
+        width: 35
     },
     followPeopleItemContent: {
-        width: '100%',
+        width: "100%",
 
-        '& > p': {
+        "& > p": {
             fontWeight: 300,
-            fontSize: '13px',
-            lineHeight: '16px',
-            margin: '4px 0 0',
-            color: '#1C1C1C',
-        },
+            fontSize: "13px",
+            lineHeight: "16px",
+            margin: "4px 0 0",
+            color: "#1C1C1C"
+        }
     },
     followPeopleItemRow: {
-        display: 'flex',
-        justifyContent: 'space-between',
+        display: "flex",
+        justifyContent: "space-between",
 
-        '& a': {
+        "& p": {
             fontWeight: 600,
-            fontSize: '15px',
-            lineHeight: '18px',
-            textDecoration: 'none',
-            color: '#1C1C1C',
+            fontSize: "15px",
+            lineHeight: "18px",
+            textDecoration: "none",
+            color: "#1C1C1C"
         },
 
-        '& small': {
+        "& small": {
             fontWeight: 300,
-            fontSize: '15px',
-            lineHeight: '18px',
-            color: '#A2A2A2',
-        },
-    },
+            fontSize: "15px",
+            lineHeight: "18px",
+            color: "#A2A2A2"
+        }
+    }
 }));
 
 const _FollowPeopleItem = ({ user, actionWithFollow, routerStore }) => {
     const classes = useStyles();
 
     return (
-        <div className={classes.followPeopleItem} key={user.id}>
-            <Link
-                view={Routes.userProfile}
-                params={{ username: user.id }}
-                store={routerStore}
-            >
-                <Avatar
-                    className={classes.followPeopleItemAvatar}
-                    src={
-                        user.avatar
-                        || 'http://localhost:3000/avatars/original/missing.png'
-                    }
-                />
-            </Link>
+        <Link
+            className={classes.followPeopleItem}
+            key={user.id}
+            view={Routes.userProfile}
+            params={{ username: user.id }}
+            store={routerStore}
+        >
+            <Avatar
+                className={classes.followPeopleItemAvatar}
+                src={
+                    user.avatar ||
+                    "http://localhost:3000/avatars/original/missing.png"
+                }
+            />
             <div className={classes.followPeopleItemContent}>
                 <div className={classes.followPeopleItemRow}>
                     <Typography>
-                        <Link
-                            view={Routes.userProfile}
-                            params={{ username: user.id }}
-                            store={routerStore}
-                        >
-                            <div>{trimString(user.display_name, 24)}</div>
-                        </Link>
-                        <small>
-                            @
-                            {trimString(user.username, 24)}
-                        </small>
+                        <div>{trimString(user.display_name, 24)}</div>
+                        <small>@{trimString(user.username, 24)}</small>
                     </Typography>
-                    <FollowButton
-                        user={user}
-                        actionWithFollow={actionWithFollow}
-                        size="lg"
-                    />
+                    <ClickEventPropagationStopper>
+                        <FollowButton
+                            user={user}
+                            actionWithFollow={actionWithFollow}
+                            size="lg"
+                        />
+                    </ClickEventPropagationStopper>
                 </div>
                 <p>{user.bio}</p>
             </div>
-        </div>
+        </Link>
     );
 };
 
 const mapMobxToProps = ({ store }) => ({
-    routerStore: store,
+    routerStore: store
 });
 
 export const FollowPeopleItem = inject(mapMobxToProps)(_FollowPeopleItem);
--- src/Settings/components/AppBarLanguageSelect.js
+++ src/Settings/components/AppBarLanguageSelect.js
@@ -1,61 +1,58 @@
-import React, { Fragment } from 'react';
-import { inject, observer } from 'mobx-react';
-import {
-    MenuItem,
-    makeStyles,
-} from '@material-ui/core';
-import MenuList from '@material-ui/core/MenuList';
-import ClickAwayListener from '@material-ui/core/ClickAwayListener';
-import Grow from '@material-ui/core/Grow';
-import Popper from '@material-ui/core/Popper';
-import Button from '@material-ui/core/Button';
-import Paper from '@material-ui/core/Paper';
-import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
-import { localized } from '../../localization/components';
+import React, { Fragment } from "react";
+import { inject, observer } from "mobx-react";
+import { MenuItem, makeStyles } from "@material-ui/core";
+import MenuList from "@material-ui/core/MenuList";
+import ClickAwayListener from "@material-ui/core/ClickAwayListener";
+import Grow from "@material-ui/core/Grow";
+import Popper from "@material-ui/core/Popper";
+import Button from "@material-ui/core/Button";
+import Paper from "@material-ui/core/Paper";
+import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
+import { localized } from "../../localization/components";
 
 const useStyles = makeStyles({
     styledSelectBox: {
-        border: 'none',
+        border: "none",
         height: 34,
         width: 34,
         borderRadius: 100,
-        color: 'rgba(0, 0, 0, 0.87)',
-        '&:hover': {
-            borderRadius: '100%',
-            background: '#FFDECC',
+        color: "rgba(0, 0, 0, 0.87)",
+        "&:hover": {
+            borderRadius: "100%",
+            background: "#FFDECC"
         },
-        '& svg': {
+        "& svg": {
             left: 35,
-            marginLeft: '10px',
+            marginLeft: "10px"
         },
-        '& span': {
-            marginLeft: '34px',
-        },
+        "& span": {
+            marginLeft: "34px"
+        }
     },
     styleMenuItem: {
-        width: 'auto',
-        margin: '0 16px',
-        padding: '16px 0',
-        minHeight: '50px',
-        borderBottom: '1px solid rgba(0,43,47,.15)',
-        textAlign: 'center',
-        '&:last-child': {
-            borderBottom: 'none',
+        width: "auto",
+        margin: "0 16px",
+        padding: "16px 0",
+        minHeight: "50px",
+        borderBottom: "1px solid rgba(0,43,47,.15)",
+        textAlign: "center",
+        "&:last-child": {
+            borderBottom: "none"
         },
-        '&:hover': {
-            background: 'rgba(255,255,255,0)',
+        "&:hover": {
+            background: "rgba(255,255,255,0)"
         },
-        '& span': {
-            color: 'rgba(255,255,255,0)',
-        },
+        "& span": {
+            color: "rgba(255,255,255,0)"
+        }
     },
     menuList: {
         padding: 0,
-        borderTop: '2px solid #131315',
+        borderTop: "2px solid #131315"
     },
     arrowAnimate: {
-        transform: 'rotate(180deg)',
-    },
+        transform: "rotate(180deg)"
+    }
 });
 
 const _AppBarLanguageSelect = ({ setSelectedLanguage, locale, l }) => {
@@ -64,7 +61,7 @@ const _AppBarLanguageSelect = ({ setSelectedLanguage, locale, l }) => {
     const anchorRef = React.useRef(null);
 
     const handleListKeyDown = event => {
-        if (event.key === 'Tab') {
+        if (event.key === "Tab") {
             event.preventDefault();
             setOpen(false);
         }
@@ -77,11 +74,15 @@ const _AppBarLanguageSelect = ({ setSelectedLanguage, locale, l }) => {
 
     return (
         <>
-            <div className={classes.styledSelectBox} onClick={() => setOpen(prevOpen => !prevOpen)}>
-                <span>{locale}</span>
-                <ArrowDropDownIcon classes={{
-                    root: open && classes.arrowAnimate,
-                }}
+            <div
+                className={classes.styledSelectBox}
+                onClick={() => setOpen(prevOpen => !prevOpen)}
+            >
+                <span>{locale.charAt(0).toUpperCase() + locale.slice(1)}</span>
+                <ArrowDropDownIcon
+                    classes={{
+                        root: open && classes.arrowAnimate
+                    }}
                 />
             </div>
             <Popper
@@ -96,9 +97,9 @@ const _AppBarLanguageSelect = ({ setSelectedLanguage, locale, l }) => {
                         {...TransitionProps}
                         style={{
                             transformOrigin:
-                                placement === 'bottom'
-                                    ? 'center top'
-                                    : 'center bottom',
+                                placement === "bottom"
+                                    ? "center top"
+                                    : "center bottom"
                         }}
                     >
                         <Paper>
@@ -112,14 +113,14 @@ const _AppBarLanguageSelect = ({ setSelectedLanguage, locale, l }) => {
                                     <MenuItem
                                         classes={{ root: classes.styleMenuItem }}
                                         value="en"
-                                        onClick={() => handleSelectLang('en')}
+                                        onClick={() => handleSelectLang("en")}
                                     >
                                         English
                                     </MenuItem>
                                     <MenuItem
                                         classes={{ root: classes.styleMenuItem }}
                                         value="ko"
-                                        onClick={() => handleSelectLang('ko')}
+                                        onClick={() => handleSelectLang("ko")}
                                     >
                                         Korean
                                     </MenuItem>
@@ -134,9 +135,9 @@ const _AppBarLanguageSelect = ({ setSelectedLanguage, locale, l }) => {
 };
 
 const mapMobxToProps = ({ localization }) => ({
-    setSelectedLanguage: localization.setSelectedLanguage,
+    setSelectedLanguage: localization.setSelectedLanguage
 });
 
 export const AppBarLanguageSelect = localized(
-    inject(mapMobxToProps)(observer(_AppBarLanguageSelect)),
+    inject(mapMobxToProps)(observer(_AppBarLanguageSelect))
 );

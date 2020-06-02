1deb2c0661aa14ab9e7e89cd10df112617c2bdea
Илья Долженко <ilay00@mail.ru>
2020-06-02 15:51:51 +0300

    notifications fixes

--- src/pages/NotificationsPage.js
+++ src/pages/NotificationsPage.js
@@ -1,16 +1,29 @@
-import React from 'react';
-import { Grid } from '@material-ui/core';
-import { observer } from 'mobx-react';
-import { AppBar } from '../AppBar/components';
-import { localized } from '../localization/components';
-import { NotificationsList } from '../Notification/components';
-import { PrometeusDescription, ExploreOurFeaturesDescription } from '../PrometeusDescription';
-import { Layout } from '../Layout';
-import { LoginForm } from '../Authorization/components';
-import { useAuthorization } from '../store/hooks';
+import React from "react";
+import { Grid, Typography, makeStyles } from "@material-ui/core";
+import { observer } from "mobx-react";
+import { AppBar } from "../AppBar/components";
+import { NotificationsList } from "../Notification/components";
+import {
+    PrometeusDescription,
+    ExploreOurFeaturesDescription
+} from "../PrometeusDescription";
+import { Layout } from "../Layout";
+import { LoginForm } from "../Authorization/components";
+import { useAuthorization, useLocalization } from "../store/hooks";
+
+const useStyles = makeStyles(theme => ({
+    notificationsTitle: {
+        fontWeight: 600,
+        fontSize: "20px",
+        lineHeight: "24px",
+        marginBottom: "24px"
+    }
+}));
 
 export const NotificationsPage = observer(() => {
+    const classes = useStyles();
     const { currentUser } = useAuthorization();
+    const { l } = useLocalization();
 
     return (
         <Grid container>
@@ -23,12 +36,23 @@ export const NotificationsPage = observer(() => {
                         <Grid item md={3} className="left-banners-container">
                             <PrometeusDescription />
                         </Grid>
-                        <Grid item spacing={28} lg={9} className="right-content-container">
+                        <Grid
+                            item
+                            spacing={28}
+                            lg={9}
+                            className="right-content-container"
+                        >
                             {!currentUser && (
                                 <Grid item className="login-form-container">
                                     <LoginForm />
                                 </Grid>
                             )}
+                            <Typography
+                                className={classes.notificationsTitle}
+                                variant="h6"
+                            >
+                                {l("appbar.notifications")}
+                            </Typography>
                             <NotificationsList />
                         </Grid>
                         <Grid item md={3} className="right-banners-container">

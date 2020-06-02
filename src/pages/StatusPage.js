473c2bd7b164a7a571d28991aea101f8295d3068
lika-alika <Banan922!>
2020-06-02 19:18:05 +0300

    statusPage - fix post container

--- src/pages/StatusPage.js
+++ src/pages/StatusPage.js
@@ -1,20 +1,44 @@
 import React from 'react';
-import { Grid } from '@material-ui/core';
+import { Grid, Hidden } from '@material-ui/core';
 import { AppBar } from '../AppBar/components';
 import { StatusBtfsInfoDialog, StatusPageContainer } from '../Status/components';
+import { ExploreOurFeaturesDescription, PrometeusDescription } from '../PrometeusDescription';
+import { WhoToFollow } from '../Follow/components';
+import { Layout } from '../Layout';
+import { inject, observer } from 'mobx-react';
 
-export const StatusPage = () => (
-    <Grid container>
-        <Grid item xs={12}>
-            <AppBar />
-        </Grid>
-        <Grid item xs={12}>
-            <Grid container className="content-container" spacing={2}>
-                <Grid item xs={12} lg={9} className="right-content-container">
-                    <StatusPageContainer />
-                </Grid>
-            </Grid>
-        </Grid>
-        <StatusBtfsInfoDialog />
+const _StatusPage = ({ currentUser }) => (
+  <Grid container>
+    <Grid item xs={12}>
+      <AppBar />
     </Grid>
+    <Grid item xs={12}>
+      <Layout>
+        <Grid container className="content-container">
+          <Grid item md={3} className="left-banners-container">
+            <PrometeusDescription />
+          </Grid>
+          <Grid item xs={12} lg={9} className="right-content-container">
+            <StatusPageContainer />
+          </Grid>
+          <Grid item md={3} className="right-banners-container">
+            {currentUser ? (
+              <Hidden only={['md']}>
+                <WhoToFollow />
+              </Hidden>
+            ) : (
+              <ExploreOurFeaturesDescription />
+            )}
+          </Grid>
+        </Grid>
+      </Layout>
+    </Grid>
+    <StatusBtfsInfoDialog />
+  </Grid>
 );
+
+const mapMobxToProps = ({ authorization }) => ({
+  currentUser: authorization.currentUser,
+});
+
+export const StatusPage = inject(mapMobxToProps)(observer(_StatusPage));

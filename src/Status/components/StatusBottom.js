b294c00524cf7d8da95e885cb95a93c112cdcdb5
Илья Долженко <ilay00@mail.ru>
2020-06-04 18:42:14 +0300

    fixed padding and rewrites status actions

--- src/Status/components/StatusBottom.js
+++ src/Status/components/StatusBottom.js
@@ -1,24 +1,18 @@
-import React, { useRef, useState } from 'react';
+import React from "react";
 import {
     CardActions,
     Checkbox,
     CircularProgress,
-    ClickAwayListener,
-    IconButton,
-    Popper,
     Typography,
-    makeStyles,
-} from '@material-ui/core';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
-import { OpenStatusBtfsInfoDialogButton } from './OpenStatusBtfsInfoDialogButton';
-import { RepostStatusMenu } from './RespostStatusMenu';
-import { CommentsButton } from './CommentsButton';
-import { LetterIcon } from '../../icons/LetterIcon';
-import { ShareIcon } from '../../icons/ShareIcon';
-import { AnotherShareIcon } from '../../icons/AnotherShareIcon';
-import { ClickEventPropagationStopper } from '../../ClickEventProgatationStopper';
-import { localized } from '../../localization/components';
+    makeStyles
+} from "@material-ui/core";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
+import { OpenStatusBtfsInfoDialogButton } from "./OpenStatusBtfsInfoDialogButton";
+import { RepostStatusMenu } from "./RepostStatusMenu";
+import { ShareStatusMenu } from "./ShareStatusMenu";
+import { CommentsButton } from "./CommentsButton";
+import { ClickEventPropagationStopper } from "../../ClickEventProgatationStopper";
 
 const useStyles = makeStyles({
     styledCheckbox: {
@@ -27,28 +21,17 @@ const useStyles = makeStyles({
         borderRadius: 100,
         width: 34,
         height: 34,
-        '&.MuiCheckbox-root': {
-            color: 'rgba(0, 0, 0, 0.35)',
+        "&.MuiCheckbox-root": {
+            color: "rgba(0, 0, 0, 0.35)"
         },
-        '&:hover': {
-            background: 'rgba(255, 92, 1, 0.2)',
-            borderRadius: 30,
-        },
-    },
-    styledIconButton: {
-        margin: 0,
-        padding: 0,
-        borderRadius: 100,
-        width: 34,
-        height: 34,
-        '&:hover': {
-            background: 'rgba(255, 92, 1, 0.2)',
-            borderRadius: 30,
-        },
+        "&:hover": {
+            background: "rgba(255, 92, 1, 0.2)",
+            borderRadius: 30
+        }
     },
     cardActionSpacing: {
-        '& > :not(:first-child)':{
-            marginLeft: '25px',
+        "& > :not(:first-child)": {
+            marginLeft: "25px"
         }
     }
 });
@@ -63,29 +46,16 @@ const _StatusBottom = ({
     repostPending,
     canBeReposted,
     currentUserIsAuthor,
-    status,
-    l,
+    status
 }) => {
     const classes = useStyles();
 
-    const [open, setOpen] = useState(false);
-    const anchorRef = useRef(null);
-
-    const handleToggle = () => {
-        setOpen(prevOpen => !prevOpen);
-    };
-
-    const handleClose = event => {
-        if (event && anchorRef.current && anchorRef.current.contains(event.target)) {
-            return;
-        }
-
-        setOpen(false);
-    };
-
     return (
         <ClickEventPropagationStopper>
-            <CardActions className="status-list-bottom-container" classes={{spacing: classes.cardActionSpacing}}>
+            <CardActions
+                className="status-list-bottom-container"
+                classes={{ spacing: classes.cardActionSpacing }}
+            >
                 <ClickEventPropagationStopper>
                     <CommentsButton status={status} />
                 </ClickEventPropagationStopper>
@@ -97,56 +67,28 @@ const _StatusBottom = ({
                 />
                 <div>
                     <ClickEventPropagationStopper className="status-list-bottom-box">
-                        {statusLikePending
-                            ? <CircularProgress size={20} color="primary" />
-                            : (
-                                <Checkbox
-                                    icon={<FavoriteBorderIcon />}
-                                    checkedIcon={<FavoriteIcon color="primary" />}
-                                    checked={favourited}
-                                    onChange={() => onFavouriteClick(statusId, !favourited)}
-                                    classes={{root:classes.styledCheckbox}}
-                                />
-                            )}
-                        <Typography variant="body1" color={favourited ? 'primary' : 'textSecondary'}>
+                        {statusLikePending ? (
+                            <CircularProgress size={20} color="primary" />
+                        ) : (
+                            <Checkbox
+                                icon={<FavoriteBorderIcon />}
+                                checkedIcon={<FavoriteIcon color="primary" />}
+                                checked={favourited}
+                                onChange={() =>
+                                    onFavouriteClick(statusId, !favourited)
+                                }
+                                classes={{ root: classes.styledCheckbox }}
+                            />
+                        )}
+                        <Typography
+                            variant="body1"
+                            color={favourited ? "primary" : "textSecondary"}
+                        >
                             {favouritesCount}
                         </Typography>
                     </ClickEventPropagationStopper>
                 </div>
-                <ClickEventPropagationStopper className="status-list-bottom-box">
-                    <div>
-                        <ClickEventPropagationStopper>
-                            <IconButton
-                                ref={anchorRef}
-                                onClick={event => {
-                                    handleToggle(event);
-                                }}
-                                classes={{root: classes.styledIconButton}}
-                                disableRipple
-                            >
-                                <AnotherShareIcon />
-                            </IconButton>
-                        </ClickEventPropagationStopper>
-                        <Popper open={open} anchorEl={anchorRef.current} role={undefined} transition>
-                            <ClickAwayListener onClickAway={handleClose}>
-                                <div className="status-list-bottom-box-modal" onClick={handleClose}>
-                                    <div className="status-modal-box-item" onClick={handleClose}>
-                                        <LetterIcon />
-                                        <Typography variant="body1" color="textSecondary">
-                                            {l('status.send-in-message')}
-                                        </Typography>
-                                    </div>
-                                    <div className="status-modal-box-item" onClick={handleClose}>
-                                        <ShareIcon />
-                                        <Typography variant="body1" color="textSecondary">
-                                            {l('status.copy-link')}
-                                        </Typography>
-                                    </div>
-                                </div>
-                            </ClickAwayListener>
-                        </Popper>
-                    </div>
-                </ClickEventPropagationStopper>
+                <ShareStatusMenu />
                 <ClickEventPropagationStopper className="status-list-bottom-box">
                     <OpenStatusBtfsInfoDialogButton btfsInfo={btfsInfo} />
                 </ClickEventPropagationStopper>
@@ -155,4 +97,4 @@ const _StatusBottom = ({
     );
 };
 
-export const StatusBottom = localized(_StatusBottom);
+export const StatusBottom = _StatusBottom;

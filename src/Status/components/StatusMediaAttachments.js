5be59eae94f25d874439ea8d35fceda8e6958f97
PaperPlane01 <dagfortnewage@gmail.com>
2020-06-19 21:03:11 +0600

    fix: (19.06.20) Fixed media attachments styling

--- src/Status/components/StatusMediaAttachments.js
+++ src/Status/components/StatusMediaAttachments.js
@@ -1,37 +1,33 @@
-import React from "react";
-import Gallery from "react-grid-gallery";
-import { makeStyles } from "@material-ui/core";
-import { ClickEventPropagationStopper } from "../../ClickEventProgatationStopper";
+import React from 'react';
+import Gallery from 'react-grid-gallery';
+import { makeStyles } from '@material-ui/core';
+import { ClickEventPropagationStopper } from '../../ClickEventProgatationStopper';
 
 const useStyles = makeStyles(theme => ({
     isMobileStatusImage: {
-        "& img": {
-            maxWidth: "100%",
-            marginLeft: "unset !important",
+        '& img': {
+            maxWidth: '100%',
+            marginLeft: 'unset !important',
 
-            [theme.breakpoints.down("450")]: {
-                width: "100% !important",
-                height: "100% !important",
-                margin: "0 !important"
-            }
-        }
-    }
+            [theme.breakpoints.down('450')]: {
+                width: '100% !important',
+                height: '100% !important',
+                margin: '0 !important',
+            },
+        },
+    },
 }));
 
-const tileViewportStyle = height => {
-    return {
-        width: height > 1000 ? "60%" : "100%",
-        height: "100%",
-        margin: "0 auto"
-    };
-};
+const tileViewportStyle = height => ({
+    width: '100%',
+    height: '100%',
+    margin: '0 auto',
+});
 
-const thumbnailStyle = () => {
-    return {
-        width: "100%",
-        height: "100%"
-    };
-};
+const thumbnailStyle = () => ({
+    width: '100%',
+    height: '100%',
+});
 
 export const StatusMediaAttachments = ({ mediaAttachments, isOnlyImage }) => {
     const classes = useStyles();
@@ -39,22 +35,20 @@ export const StatusMediaAttachments = ({ mediaAttachments, isOnlyImage }) => {
     const gallery = mediaAttachments.map(mediaAttachment => ({
         src: mediaAttachment.url,
         thumbnail: mediaAttachment.url,
-        thumbnailWidth: mediaAttachment.meta.width,
-        thumbnailHeight: mediaAttachment.meta.height
+        thumbnailWidth: mediaAttachments.length === 1 ? '100%' : mediaAttachment.meta.width,
+        thumbnailHeight: mediaAttachment.meta.height,
     }));
 
     return (
-        <div style={{ display: "flex" }}>
-            <div style={{ flex: "auto" }} className={classes.isMobileStatusImage}>
+        <div style={{ display: 'flex' }}>
+            <div style={{ flex: 'auto' }} className={classes.isMobileStatusImage}>
                 <ClickEventPropagationStopper>
                     <Gallery
                         images={gallery}
                         enableImageSelection={false}
-                        thumbnailStyle={isOnlyImage && thumbnailStyle}
-                        tileViewportStyle={() =>
-                            tileViewportStyle(gallery[0].thumbnailHeight)
-                        }
+                        thumbnailStyle={mediaAttachments.length === 1 ? thumbnailStyle : undefined}
                         showLightboxThumbnails
+                        rowHeight={mediaAttachments.length === 1 ? '100%' : 180}
                     />
                 </ClickEventPropagationStopper>
             </div>

36cba6428dd5751f781fe22c0c5631c6cb999d7c
Илья Долженко <ilay00@mail.ru>
2020-06-19 11:37:14 +0300

    remove emoji-mart

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { MuiThemeProvider } from '@material-ui/core';
 import { SnackbarProvider } from 'notistack';
 import { MobxRouter } from 'mobx-router';
 import './styles/App.sass';
-import "emoji-mart/css/emoji-mart.css";
 import { orange } from './themes/orange';
 
 export const App = () => (

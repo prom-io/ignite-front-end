4de418449ff3d5a08da87d2cc0879b196e045305
lika-alika <Banan922!>
2020-07-01 18:06:38 +0300

    follow item

--- src/Follow/components/FollowPeopleItem.js
+++ src/Follow/components/FollowPeopleItem.js
@@ -78,10 +78,10 @@ const _FollowPeopleItem = ({ user, actionWithFollow, routerStore }) => {
             <div className={classes.followPeopleItemContent}>
                 <div className={classes.followPeopleItemRow}>
                     <Typography>
-                        <div>{trimString(user.display_name, 12)}</div>
+                        <div>{trimString(user.display_name, 16)}</div>
                         <small>
                             @
-                            {trimString(user.username, 12)}
+                            {trimString(user.username, 16)}
                         </small>
                     </Typography>
                     <ClickEventPropagationStopper>

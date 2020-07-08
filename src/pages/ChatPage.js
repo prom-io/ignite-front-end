c40a5bed59cd3a7de140ad0707f6118c529a160e
Илья Долженко <ilay00@mail.ru>
2020-07-08 14:55:30 +0300

    terms of services styles

--- src/pages/ChatPage.js
+++ src/pages/ChatPage.js
@@ -1,45 +1,48 @@
-import React from 'react';
-import { Grid } from '@material-ui/core';
-import { inject, observer } from 'mobx-react';
+import React from "react";
+import { Grid } from "@material-ui/core";
+import { inject, observer } from "mobx-react";
 
-import { makeStyles } from '@material-ui/core/styles';
-import { localized } from '../localization/components';
-import { AppBar } from '../AppBar/components';
+import { makeStyles } from "@material-ui/core/styles";
+import { localized } from "../localization/components";
+import { AppBar } from "../AppBar/components";
 import {
     PrometeusDescription,
-    ExploreOurFeaturesDescription,
-} from '../PrometeusDescription';
-import { Layout } from '../Layout';
-import { LoginForm } from '../Authorization/components';
+    ExploreOurFeaturesDescription
+} from "../PrometeusDescription";
+import { Layout } from "../Layout";
+import { LoginForm } from "../Authorization/components";
 
 const useStyles = makeStyles(theme => ({
     chatError: {
         border: `1px solid ${theme.palette.border.main}`,
-        height: '100%',
-        padding: '30px',
-        textAlign: 'center',
-        '& h1': {
-            fontFamily: 'Museo Sans Cyrl Bold',
-            fontSize: '20px',
-            margin: '24px 0 4px 0',
-            color: theme.palette.text.main,
+        height: "100%",
+        padding: "30px",
+        textAlign: "center",
+        "& h1": {
+            fontFamily: "Museo Sans Cyrl Bold",
+            fontSize: "20px",
+            margin: "24px 0 4px 0",
+            color: theme.palette.text.main
         },
-        '& p': {
+        "& p": {
             color: theme.palette.text.secondary,
-            fontSize: '15px',
-            lineHeight: '26px',
+            fontSize: "15px",
+            lineHeight: "26px"
         }
     },
-    chatErrorInfo: {
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        fontFamily: 'Museo Sans Cyrl Regular',
-        fontSize: '15px',
-        lineHeight: '26px',
-        color: '#A2A2A2',
+    chatErrorTitle: {
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        fontFamily: "Museo Sans Cyrl Regular",
+        fontSize: "15px",
+        lineHeight: "26px",
+        color: "#A2A2A2"
     },
+    chatErrorInfo: {
+        textAlign: "justify"
+    }
 }));
 
 const _ChatPage = ({ currentUser, l }) => {
@@ -63,17 +66,23 @@ const _ChatPage = ({ currentUser, l }) => {
                         >
                             {!currentUser && (
                                 <Grid item className="login-form-container">
-                                    <LoginForm hideSignUpButton={process.env.REACT_APP_HIDE_SIGN_UP_BUTTON === 'true'} />
+                                    <LoginForm
+                                        hideSignUpButton={
+                                            process.env
+                                                .REACT_APP_HIDE_SIGN_UP_BUTTON ===
+                                            "true"
+                                        }
+                                    />
                                 </Grid>
                             )}
                             <div className={classes.chatError}>
-                                <div className={classes.chatErrorInfo}>
+                                <div className={classes.chatErrorTitle}>
                                     <div className="ignite-chat-page" />
                                     <h1>PrompTalk Chat</h1>
                                 </div>
-                                <div>
-                                    <p>{l('chat.description.first-paragraph')}</p>
-                                    <p>{l('chat.description.second-paragraph')}</p>
+                                <div className={classes.chatErrorInfo}>
+                                    <p>{l("chat.description.first-paragraph")}</p>
+                                    <p>{l("chat.description.second-paragraph")}</p>
                                 </div>
                             </div>
                         </Grid>
@@ -88,7 +97,7 @@ const _ChatPage = ({ currentUser, l }) => {
 };
 
 const mapMobxToProps = ({ authorization }) => ({
-    currentUser: authorization.currentUser,
+    currentUser: authorization.currentUser
 });
 
 export const ChatPage = localized(inject(mapMobxToProps)(observer(_ChatPage)));
--- src/pages/TermsAndPoliciesPage.js
+++ src/pages/TermsAndPoliciesPage.js
@@ -1,85 +1,123 @@
-import React from 'react';
-import { Grid } from '@material-ui/core';
-import { AppBar } from '../AppBar/components';
-import { StaticPageFooter } from '../components/StaticPageFooter';
-import { localized } from '../localization/components';
-import { Layout } from '../Layout';
+import React from "react";
+import { inject, observer } from "mobx-react";
+import { Grid } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
-const _TermsAndPolicesPage = ({ l }) => (
-    <div className="static-page">
-        <AppBar currentActiveRoute="terms" />
-            <Grid container className="chat-container">
-                <div className="static-page-container">
-                    <div>
-                        <div className="static-page-logo-container">
-                            <img src="/page_img/terms_of_service_page.svg" />
-                            <h1>{l('terms-of-service')}</h1>
-                        </div>
-                        <div>
-                            <p>
-                                {l('terms-of-service.paragraph-1')}
-                            </p>
-                            <p>
-                                {l('terms-of-service.paragraph-2')}
-                            </p>
-                            <p>
-                                {l('terms-of-service.paragraph-3')}
-                            </p>
-                            <p>
-                                {l('terms-of-service.paragraph-4')}
-                            </p>
-                            <p>
-                                {l('terms-of-service.paragraph-5')}
-                            </p>
-                            <p>
-                                {l('terms-of-service.paragraph-6')}
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div className="static-page-container">
-                    <div>
-                        <div className="static-page-logo-container">
-                            <img src="/page_img/privacy_policies_page.svg" />
-                            <h1>{l('privacy-policies')}</h1>
-                        </div>
-                        <div>
-                            <p>
-                                {l('privacy-policies.version')}
-                                {l('privacy-policies.paragraph-1')}
-                            </p>
-                            <p>
-                                {l('privacy-policies.paragraph-2')}
-                            </p>
-                            <p>
-                                {l('privacy-policies.paragraph-3')}
-                            </p>
-                            <p>
-                                {l('privacy-policies.paragraph-4')}
-                            </p>
-                            <p>
-                                {l('privacy-policies.paragraph-5')}
-                            </p>
-                            <p>
-                                {l('privacy-policies.paragraph-6')}
-                            </p>
-                            <p>
-                                {l('privacy-policies.paragraph-7')}
-                            </p>
-                            <p>
-                                {l('privacy-policies.paragraph-8')}
-                            </p>
-                            <p>
-                                {l('privacy-policies.paragraph-9')}
-                            </p>
-                            <p>
-                                {l('privacy-policies.paragraph-10')}
-                            </p>
-                        </div>
-                    </div>
-                </div>
+import { localized } from "../localization/components";
+import { AppBar } from "../AppBar/components";
+import {
+    PrometeusDescription,
+    ExploreOurFeaturesDescription
+} from "../PrometeusDescription";
+import { Layout } from "../Layout";
+import { LoginForm } from "../Authorization/components";
+
+const useStyles = makeStyles(theme => ({
+    termsAndPolicies: {
+        border: `1px solid ${theme.palette.border.main}`,
+        height: "100%",
+        padding: "30px",
+        textAlign: "center",
+        "& h1": {
+            fontFamily: "Museo Sans Cyrl Bold",
+            fontSize: "20px",
+            margin: "24px 0 4px 0",
+            color: theme.palette.text.main
+        },
+        "& p": {
+            color: theme.palette.text.secondary,
+            fontSize: "15px",
+            lineHeight: "26px"
+        }
+    },
+    termsAndPoliciesTitle: {
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        fontFamily: "Museo Sans Cyrl Regular",
+        fontSize: "15px",
+        lineHeight: "26px",
+        color: "#A2A2A2"
+    },
+    termsAndPoliciesInfo: {
+        textAlign: "justify",
+        marginBottom: "50px",
+        "&:last-child": {
+            marginBottom: 0
+        }
+    }
+}));
+
+const _TermsAndPolicesPage = ({ currentUser, l }) => {
+    const classes = useStyles();
+
+    return (
+        <Grid container>
+            <Grid item xs={12}>
+                <AppBar currentActiveRoute="terms" />
             </Grid>
-    </div>
-);
+            <Grid item xs={12}>
+                <Layout>
+                    <Grid container className="content-container">
+                        <Grid item md={3} className="left-banners-container">
+                            <PrometeusDescription />
+                        </Grid>
+                        <Grid
+                            item
+                            spacing={28}
+                            lg={9}
+                            className="right-content-container"
+                        >
+                            {!currentUser && (
+                                <Grid item className="login-form-container">
+                                    <LoginForm
+                                        hideSignUpButton={
+                                            process.env
+                                                .REACT_APP_HIDE_SIGN_UP_BUTTON ===
+                                            "true"
+                                        }
+                                    />
+                                </Grid>
+                            )}
+                            <div className={classes.termsAndPolicies}>
+                                <div className={classes.termsAndPoliciesTitle}>
+                                    <img src="/page_img/terms_of_service_page.svg" />
+                                    <h1>{l("terms-of-service")}</h1>
+                                </div>
+                                <div className={classes.termsAndPoliciesInfo}>
+                                    <p>{l("terms-of-service.paragraph-1")}</p>
+                                    <p>{l("terms-of-service.paragraph-2")}</p>
+                                    <p>{l("terms-of-service.paragraph-3")}</p>
+                                    <p>{l("terms-of-service.paragraph-4")}</p>
+                                </div>
+                                <div className={classes.termsAndPoliciesTitle}>
+                                    <img src="/page_img/privacy_policies_page.svg" />
+                                    <h1>{l("privacy-policies")}</h1>
+                                </div>
+                                <div className={classes.termsAndPoliciesInfo}>
+                                    <p>{l("privacy-policies.paragraph-1")}</p>
+                                    <p>{l("privacy-policies.paragraph-2")}</p>
+                                    <p>{l("privacy-policies.paragraph-3")}</p>
+                                    <p>{l("privacy-policies.paragraph-4")}</p>
+                                    <p>{l("privacy-policies.paragraph-5")}</p>
+                                </div>
+                            </div>
+                        </Grid>
+                        <Grid item md={3} className="right-banners-container">
+                            <ExploreOurFeaturesDescription />
+                        </Grid>
+                    </Grid>
+                </Layout>
+            </Grid>
+        </Grid>
+    );
+};
 
-export const TermsAndPoliciesPage = localized(_TermsAndPolicesPage);
+const mapMobxToProps = ({ authorization }) => ({
+    currentUser: authorization.currentUser
+});
+
+export const TermsAndPoliciesPage = localized(
+    inject(mapMobxToProps)(observer(_TermsAndPolicesPage))
+);

63ab7b7bdee60de37269cb11ce80eb30e3fbcbc8
lika-alika <Banan922!>
2020-07-24 16:26:37 +0300

    fix stoa block for mobile

--- src/User/components/UserProfileContainer.js
+++ src/User/components/UserProfileContainer.js
@@ -79,7 +79,9 @@ const _UserProfileContainer = ({
                     currentUserFollowingCount={currentUser && currentUser.follows_count}
                     createdAt={new Date(user.created_at)}
                 />
+                <Hidden smDown>
                 <DescriptionStoaBanner />
+                </Hidden>
             </Grid>
             <Grid item className="user-profile-content-container">
                 {tabContent}

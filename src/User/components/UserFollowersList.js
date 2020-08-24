a71acbc70467255aa68f9afdf107e86c4f912868
Илья Долженко <ilay00@mail.ru>
2020-08-24 12:28:43 +0300

    fixed following list loader

--- src/User/components/UserFollowersList.js
+++ src/User/components/UserFollowersList.js
@@ -1,29 +1,33 @@
-import React from 'react';
-import { inject, observer } from 'mobx-react';
-import { makeStyles } from '@material-ui/core';
+import React from "react";
+import { inject, observer } from "mobx-react";
+import { makeStyles } from "@material-ui/core";
 
-import { UsersList } from './UsersList';
-import { UserEmptyList } from './UserEmptyList';
-import Loader from '../../components/Loader';
+import { UsersList } from "./UsersList";
+import { UserEmptyList } from "./UserEmptyList";
+import Loader from "../../components/Loader";
 
 const useStyles = makeStyles(() => ({
     centered: {
-        marginLeft: 'auto',
-        marginRight: 'auto',
-        marginTop: '150px',
-        display: 'table',
-    },
+        marginLeft: "auto",
+        marginRight: "auto",
+        marginTop: "150px",
+        display: "table"
+    }
 }));
 
 const _UserFollowersList = ({ followers, pending, hasMore, fetchFollowers }) => {
     const classes = useStyles();
 
-    return (pending && followers.length === 0 && hasMore) ? (
+    return pending && followers.length === 0 ? (
         <div className={classes.centered}>
             <Loader size="lg" />
         </div>
     ) : followers.length ? (
-        <UsersList users={followers} hasMore={hasMore} onNextPageRequest={fetchFollowers} />
+        <UsersList
+            users={followers}
+            hasMore={hasMore}
+            onNextPageRequest={fetchFollowers}
+        />
     ) : (
         <UserEmptyList emptyTag="followers" />
     );
@@ -33,9 +37,9 @@ const mapMobxToProps = ({ userFollowers }) => ({
     pending: userFollowers.pending,
     followers: userFollowers.followers,
     hasMore: userFollowers.hasMore,
-    fetchFollowers: userFollowers.fetchUserFollowers,
+    fetchFollowers: userFollowers.fetchUserFollowers
 });
 
 export const UserFollowersList = inject(mapMobxToProps)(
-    observer(_UserFollowersList),
+    observer(_UserFollowersList)
 );
--- src/User/components/UserFollowingList.js
+++ src/User/components/UserFollowingList.js
@@ -1,29 +1,33 @@
-import React from 'react';
-import { inject, observer } from 'mobx-react';
-import { makeStyles } from '@material-ui/core';
+import React from "react";
+import { inject, observer } from "mobx-react";
+import { makeStyles } from "@material-ui/core";
 
-import { UsersList } from './UsersList';
-import { UserEmptyList } from './UserEmptyList';
-import Loader from '../../components/Loader';
+import { UsersList } from "./UsersList";
+import { UserEmptyList } from "./UserEmptyList";
+import Loader from "../../components/Loader";
 
 const useStyles = makeStyles(() => ({
     centered: {
-        marginLeft: 'auto',
-        marginRight: 'auto',
-        marginTop: '150px',
-        display: 'table',
-    },
+        marginLeft: "auto",
+        marginRight: "auto",
+        marginTop: "150px",
+        display: "table"
+    }
 }));
 
 const _UserFollowingList = ({ following, pending, hasMore, fetchFollowing }) => {
     const classes = useStyles();
 
-    return pending ? (
+    return pending && following.length === 0 ? (
         <div className={classes.centered}>
             <Loader size="lg" />
         </div>
     ) : following.length ? (
-        <UsersList users={following} hasMore={hasMore} onNextPageRequest={fetchFollowing} />
+        <UsersList
+            users={following}
+            hasMore={hasMore}
+            onNextPageRequest={fetchFollowing}
+        />
     ) : (
         <UserEmptyList emptyTag="following" />
     );
@@ -33,9 +37,9 @@ const mapMobxToProps = ({ userFollowing }) => ({
     pending: userFollowing.pending,
     following: userFollowing.following,
     fetchFollowing: userFollowing.fetchFollowing,
-    hasMore: userFollowing.hasMore,
+    hasMore: userFollowing.hasMore
 });
 
 export const UserFollowingList = inject(mapMobxToProps)(
-    observer(_UserFollowingList),
+    observer(_UserFollowingList)
 );

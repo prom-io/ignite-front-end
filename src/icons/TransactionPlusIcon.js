5d81fdb05f63d425e663dbd617e3e8e2fe1c5ca9
Илья Долженко <ilay00@mail.ru>
2020-10-06 18:45:14 +0300

    added new transaction icons

---
+++ src/icons/TransactionPlusIcon.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export const TransactionPlusIcon = () => (
+    <svg
+        width="16"
+        height="16"
+        viewBox="0 0 16 16"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+    >
+        <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M8 0C7.44772 0 7 0.477563 7 1.06667V7H1.06667C0.477563 7 0 7.44772 0 8C0 8.55228 0.477563 9 1.06667 9H7V14.9333C7 15.5224 7.44772 16 8 16C8.55228 16 9 15.5224 9 14.9333V9H14.9333C15.5224 9 16 8.55229 16 8C16 7.44772 15.5224 7 14.9333 7H9V1.06667C9 0.477563 8.55228 0 8 0Z"
+            fill="#27AE60"
+        />
+    </svg>
+);
